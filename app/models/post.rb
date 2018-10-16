3ef0f9ec0c6c7e4d2e56bd37fa8b2a75f18ebd70
Daniel Schoch <d.schoch90@gmail.com>
2018-10-16 17:58:40 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,2 @@
 class Post < ActiveRecord::Base
-  #attr_accessor :name, :content
-
-  #POSTS = []
-
-  #def initialize(name:, content:)
-    #@name = name
-    #@content = content
-  #end
-
-  #def self.create(name, content)
-    #post = self.new(name, content)
-
-    #POSTS << post
-  #end
 end
